376efcc3a199a13c7dc103d0b41893e7bd0a0b42
Baron <04170133@gm.scu.edu.tw>
2021-06-08 20:08:51 +0800

    add nav

--- mainsite/views.py
+++ mainsite/views.py
@@ -4,35 +4,6 @@ from .models import Post
 from datetime import datetime
 # Create your views here.
 
-def homepage(request):
-    posts = Post.objects.all()
-    now = datetime.now()
-    return render(request, 'index.html', locals())
-
-def showpost(request, slug):
-    try:
-        post = Post.objects.get(slug=slug)
-        if post !=None:
-            return render(request, 'post.html', locals())
-    except:
-        return redirect('/')
-
-def about(request):
-    quote = 'Give more than you planned to.'
-    return render(request, 'about.html', locals())
-
-def listing(request):
-    products = Post.objects.all()
-    return render(request, 'list.html', locals())
-
-def disp_detail(request, title):
-    try:
-        p = Post.objects.get(title=title)
-    except Post.DoesNotExist:
-        raise Http404('找不到歌曲')
-
-    return render(request, 'disp.html', locals())
-
 def index(request):
     quote = 'Give more than you planned to.'
-    return render(request, 'index_ugly.html', locals())
+    return render(request, 'index.html', locals())
--- mblog/urls.py
+++ mblog/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from mainsite.views import homepage, showpost, about, listing, disp_detail, index
+from django.urls import path, include 
+from mainsite.views import index 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('post/<slug:slug>/', showpost),
-    path('about/', about),
-    path('songlist/', listing), 
-    path('songlist/<str:title>', disp_detail), 
 ]
