37a954646165b372f8bd286609c695c3645d182d
Baron <04170133@gm.scu.edu.tw>
2021-06-11 17:33:12 +0800

    fixed navbar & add myinfo

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Post
 from datetime import datetime
+from django.views.decorators.cache import never_cache
 # Create your views here.
 
+@never_cache
 def index(request):
     quote = 'Give more than you planned to.'
     return render(request, 'index.html', locals())
