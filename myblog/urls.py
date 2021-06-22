fd24512cc035b18654031c1daf9ce531afc8a309
Baron <04170133@gm.scu.edu.tw>
2021-06-22 13:05:38 +0800

    mblog replace to myblog

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,4 +1,4 @@
-"""mblog URL Configuration
+"""myblog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
